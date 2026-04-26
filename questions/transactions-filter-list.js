9f8d5617db6098d14a1dfb6485ed670b6cefc2f4
Robson Locatelli <rlocatelli9@gmail.com>
2026-04-26 14:14:31 -0300

    feat: Implement debounce functionality and enhance transaction filtering UI

--- questions/transactions-filter-list.js
+++ questions/transactions-filter-list.js
@@ -1,3 +1,40 @@
+function createDebounce() {
+  let timer = null;
+
+  return (callback, timeout = 500) => {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      callback();
+    }, timeout);
+  };
+}
+
+function appendChildElement(elementList){
+  let transactionsList = document.querySelector('ul#filtered-list')
+  if(transactionsList){
+    document.querySelectorAll('li').forEach(item => {
+      if(item.parentNode === transactionsList) transactionsList.removeChild(item)
+    })
+  } else {
+    transactionsList = document.createElement('ul')
+    transactionsList.setAttribute('id', 'filtered-list')
+    transactionsList.setAttribute('aria-label', 'transactions list')
+    let content = document.querySelector('div#content')
+    content.appendChild(transactionsList) 
+  }
+  transactionsList.setAttribute('style', 'height: 30rem; overflow-y: auto')
+
+  elementList.forEach(transaction => {
+  const li = document.createElement('li')
+  li.setAttribute('aria-label', `transaction-${transaction.name||'not-found'}`)
+  li.setAttribute('id', `transaction-${transaction.id}`)
+  const span = document.createElement('span')
+  span.textContent = transaction.description
+  li.appendChild(span)
+  transactionsList.appendChild(li)
+})}
+
 const transactions = Array.from({ length: 5000 }, (_, i) => {
   const types = ['Pagamento', 'Transferência', 'Depósito', 'Estorno'];
   const categories = [
@@ -23,4 +60,34 @@ const transactions = Array.from({ length: 5000 }, (_, i) => {
   };
 });
 
+const debounce = createDebounce()
+let inputValue = null
+let filteredList = []
+
+appendChildElement(transactions)
+
+const inputElement = document.querySelector('input#search-field')
+inputElement.addEventListener('keydown', (event) => {
+  debounce(() => {
+    inputValue = event.target.value
+    filteredList = transactions.filter(item => 
+      item
+      .description
+      .toLocaleLowerCase()
+      .includes(inputValue.toLocaleLowerCase())
+    )
+    if(filteredList.length){
+      appendChildElement(filteredList)
+    } else {
+      if(inputValue) {
+        appendChildElement([{
+          id: 1,
+          description: 'Nenhum resultado',
+        }])
+      } else {
+        appendChildElement(transactions)
+      }
+    }
+  })
+})
 
